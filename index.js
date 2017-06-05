33979e0c2b28b2e096db1a5cd144413c22348ac0
Andreas Holstenson <a@holstenson.se>
2017-06-05 17:20:06 +0200

    Spaces to tabs in index.js

--- index.js
+++ index.js
@@ -6,31 +6,31 @@ const BoundedCache = require('./cache/bounded');
  * Builder for cache instances.
  */
 class Builder {
-    constructor() {
-        this.options = {};
-    }
+	constructor() {
+		this.options = {};
+	}
 
-    /**
-     * Set the maximum number of items to keep in the cache before evicting
-     * something.
-     */
-    withMaxSize(size) {
-        this.options.maxSize = size;
-        return this;
-    }
+	/**
+	 * Set the maximum number of items to keep in the cache before evicting
+	 * something.
+	 */
+	withMaxSize(size) {
+		this.options.maxSize = size;
+		return this;
+	}
 
-    /**
-     * Build and return the cache.
-     */
-    build() {
-        if(! this.options.maxSize) {
-            throw new Error('Caches without maximum size are currently not supported');
-        }
+	/**
+	 * Build and return the cache.
+	 */
+	build() {
+		if(! this.options.maxSize) {
+			throw new Error('Caches without maximum size are currently not supported');
+		}
 
-        return new BoundedCache(this.options);
-    }
+		return new BoundedCache(this.options);
+	}
 }
 
 module.exports = function() {
-    return new Builder();
+	return new Builder();
 }
