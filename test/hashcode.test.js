473ed54dc0e32da8bfb31317cd7866ff66793f81
Andreas Holstenson <a@holstenson.se>
2017-06-10 22:27:42 +0200

    Using MurmurHash3 to calculate hashcodes

--- test/hashcode.test.js
+++ test/hashcode.test.js
@@ -5,7 +5,11 @@ const hashcode = require('../utils/hashcode');
 
 describe('Hash codes', function() {
 	it('Calculates for string', function() {
-		expect(hashcode('key')).to.not.be.null;
+		expect(hashcode('')).to.equal(0);
+		expect(hashcode('', 1)).to.equal(0x514E28B7);
+		expect(hashcode('abc')).to.equal(0xB3DD93FA);
+		expect(hashcode('aaaa', 0x9747b28c)).to.equal(0x5A97808A);
+		expect(hashcode('Hello, world!', 0x9747b28c)).to.equal(0x24884CBA);
 	});
 
 	it('Calculates for number', function() {
--- utils/hashcode.js
+++ utils/hashcode.js
@@ -1,26 +1,84 @@
 'use strict';
 
+/*eslint no-fallthrough: "off"*/
+
+const C1 = 0xcc9e2d51;
+const C2 = 0x1b873593;
+
+function safeishMultiply(a, b) {
+	return ((a & 0xffff) * b) + ((((a >>> 16) * b) & 0xffff) << 16);
+}
+
 /**
  * Utility for calculating stable hashcodes for keys used in a cache.
  */
-module.exports = function hashcode(obj) {
+module.exports = function hashcode(obj, seed=0) {
 	switch(typeof obj) {
 		case 'string':
 		{
-			let hash = 5381;
-			for(let i=0, n=obj.length; i<n; i++) {
-				hash = (hash * 33) ^ obj.charCodeAt(i);
+			let hash = seed;
+			let n = obj.length & ~0x3;
+			for(let i=0; i<n; i+=4) {
+				let k1 = ((obj.charCodeAt(i) & 0xffff)) |
+					((obj.charCodeAt(i + 1) & 0xffff) << 8) |
+					((obj.charCodeAt(i + 2) & 0xffff) << 16) |
+					((obj.charCodeAt(i + 3) & 0xffff) << 24);
+
+				k1 = safeishMultiply(k1, C1);
+				k1 = (k1 << 15) | (k1 >>> 17);
+				k1 = safeishMultiply(k1, C2);
+
+				hash ^= k1;
+				hash = (hash << 13) | (hash >>> 19);
+				hash = hash * 5 + 0xe6546b64;
 			}
-			hash >>> 0;
-			return hash;
+
+			let k1 = 0;
+			switch(obj.length & 3) {
+				case 3:
+					k1 ^= (obj.charCodeAt(n + 2) & 0xffff) << 16;
+				case 2:
+					k1 ^= (obj.charCodeAt(n + 1) & 0xffff) << 8;
+				case 1:
+					k1 ^= (obj.charCodeAt(n) & 0xffff);
+
+					k1 = safeishMultiply(k1, C1);
+					k1 = (k1 << 15) | (k1 >>> 17);
+					k1 = safeishMultiply(k1, C2);
+
+					hash ^= k1;
+			}
+
+			hash ^= obj.length;
+
+			hash ^= hash >>> 16;
+			hash = safeishMultiply(hash, 0x85ebca6b);
+			hash ^= hash >>> 13;
+			hash = safeishMultiply(hash, 0xc2b2ae35);
+			hash ^= hash >>> 16;
+
+			return hash >>> 0;
 		}
 		case 'number':
 		{
 			let hash = obj;
-			hash = ((hash >> 16) ^ hash) * 0x45d9f3b;
-			hash = ((hash >> 16) ^ hash) * 0x45d9f3b;
-			hash = ((hash >> 16) ^ hash);
-			return hash;
+
+			hash = safeishMultiply(hash, C1);
+			hash = (hash << 15) | (hash >>> 17);
+			hash = safeishMultiply(hash, C2);
+
+			hash = (hash << 13) | (hash >>> 19);
+			hash = hash * 5 + 0xe6546b64;
+
+			hash ^= hash >>> 16;
+			hash = safeishMultiply(hash, 0x85ebca6b);
+			hash ^= hash >>> 13;
+			hash = safeishMultiply(hash, 0xc2b2ae35);
+			hash ^= hash >>> 16;
+
+			hash ^= 1;
+
+			return hash >>> 0;
 		}
 		case 'boolean':
 		{
